34aa3850acd4b44f61a77ba49aa4a0d3a93bb59b
srj-17 <saugatrijal17@gmail.com>
2025-04-17 11:37:47 +0545

    fix: Don't use ssl mode for local host
    Pg doesn't agree using self-signed certificates
    for connection.

--- db/pool.js
+++ db/pool.js
@@ -3,9 +3,9 @@ const { Pool } = require("pg");
 module.exports = new Pool({
   // other settings come from environment variables
   // make the connection to database server secure
-  // with ssl
-  ssl: {
-    require: true,
-    rejectUnauthorized: false,
-  },
+  // with
+  // Doesn't work on local hosting
+  //ssl: {
+  //  require: true,
+  //},
 });
